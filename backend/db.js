318b0409893da34b13f7c750233797311edd1210
Thiago Torres <torrestube093@gmail.com>
2025-04-21 21:55:26 -0300

    render api

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,14 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false, // necessário para o Render
+  },
 });
 
 export default pool;
